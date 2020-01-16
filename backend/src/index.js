8ec1e45bfc64b8af58e00e332eda1e4e52898894
Fábio Donizete <fabio.donizete@yahoo.com.br>
2020-01-16 14:25:23 -0300

    15/01 - Construindo a interface web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -8,7 +9,7 @@ mongoose.connect('mongodb+srv://jinturgon:playsasion@cluster0-uqtbc.mongodb.net/
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-
+app.use(cors())
 app.use(express.json());
 app.use(routes); 
 
