22c38644b22faf458d389765fca5030a9a62e0df
Fábio Donizete <fabio.donizete@yahoo.com.br>
2020-01-17 14:40:09 -0300

    17/01 - Funcionalidades avançadas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://jinturgon:playsasion@cluster0-uqtbc.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -13,5 +18,5 @@ app.use(cors())
 app.use(express.json());
 app.use(routes); 
 
-app.listen(3333);
+server.listen(3333);
 
